b3bd4cacf4332418c45eff76e78942316c1e5396
tamuatsu <70050100+tamuatsu@users.noreply.github.com>
2020-09-29 07:34:13 +0900

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const morgan = require('morgan');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const { join } = require('path');
+
+console.log(process.env.POSTGRES_USER);
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : '',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+const app = express();
+app.use(cors())
+app.use(bodyParser.json());
+app.get('/', (req, res)=> { res.send(db.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+})
